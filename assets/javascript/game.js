106db336a4d91747326e6b8f2e8c6b3d8eb4d87c
Jack <jdempsey1993@gmail.com>
2019-07-17 15:50:57 -0400

    Pseudocode for javascipt functions of the game.

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+// Create a word array for game vocabulary
+
+var words = ["monkey"]
+console.log("monkey")
+
+//Choose word
+
+//Display blanks to user "_"
+
+//Keypress function for letter input
+
+//Update guess - submit letter value 
+
+    //Blank or non letter input - loop break
+
+    //More than one letter - alert user "One letter"
+
+    //One letter guess correct 
+
+        // Update correctLetters using documentgetelementbyid
+    
+    //One letter guess incorrect
+
+        // Update lettersGuessed using documentgetelementbyid
+
+// When letter values match word chosen
+
+        // Update numberOfWinds +1
+
+        //Chose another word from the array
